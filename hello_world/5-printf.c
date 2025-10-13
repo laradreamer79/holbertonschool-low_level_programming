5dfde967207ad1a6572c6d84199c4c5703eda2e1
laradreamer79 <laradreamer79@gmail.com>
2025-10-13 09:53:15 +0000

    a

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include < studio.h >
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	printf("with proper grammer, but the output is a piece of art", \n);
+	Return (0);
+}
+
