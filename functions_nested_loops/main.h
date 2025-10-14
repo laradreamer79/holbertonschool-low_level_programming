d3762209298d05eee53367e15cd380134032c6cb
laradreamer79 <laradreamer79@gmail.com>
2025-10-14 19:30:22 +0000

    a

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Your Name
+ * Desc: Header file containing declarations for all functions.
+ */
+
 int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
 
