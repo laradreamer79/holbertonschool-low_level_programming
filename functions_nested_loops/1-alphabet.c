fb7a43e1b4672870d8f6d46f738142c3ad9bf2d1
laradreamer79 <laradreamer79@gmail.com>
2025-10-14 19:51:00 +0000

    r

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet - prints the alphabet in lowercase,
  * followed by a new line
- *
  * Return: void
  */
 void print_alphabet(void)
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Your Name
- * Desc: Header file containing declarations for all functions.
- */
-
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
 
