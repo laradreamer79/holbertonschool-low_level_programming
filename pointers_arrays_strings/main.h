6a3dbb6fbc07e7759011a226cc91c0a042645c2f
laradreamer79 <laradreamer79@gmail.com>
2025-10-22 08:48:19 +0000

    s

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Author: Lara (your name if required)
+ * Description: Header file containing function prototypes
+ *              used in 0x05-pointers_arrays_strings project.
+ */
 
 void reset_to_98(int *n);
 
