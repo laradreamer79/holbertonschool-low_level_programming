3b9b195cc8c43c8fdcde4b4f44afd43dfc5ceb6a
laradreamer79 <laradreamer79@gmail.com>
2025-10-22 10:12:50 +0000

    s

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -8,13 +8,13 @@
  */
 void _puts(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
 
