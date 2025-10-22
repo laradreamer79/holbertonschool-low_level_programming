9b5694f56f01d46e5423f6b3511f7eb7c76f4402
laradreamer79 <laradreamer79@gmail.com>
2025-10-22 10:04:14 +0000

    s

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,8 +8,10 @@
  *              used in 0x05-pointers_arrays_strings project.
  */
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 #endif
 
