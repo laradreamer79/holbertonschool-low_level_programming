8b4156f05bde9722e6bfafe0a1cd073e2b5ee47b
laradreamer79 <laradreamer79@gmail.com>
2025-10-23 08:53:40 +0000

    print every other number

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *puts2 - print every other character of a string followed by a new line.
+ *
+ *
+ *@str: the string
+ */
+
+	void puts2(char *str)
+	{
+	int i = 0;
+
+
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+	_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+
+	}
