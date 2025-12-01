466d3be638aeaa01278d20453725e186cacdfdf9
laradreamer79 <laradreamer79@gmail.com>
2025-12-01 12:07:33 +0000

    fix betty style

--- file_io/main.h
+++ file_io/main.h
@@ -1,22 +1,45 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * struct fileio - File I/O utilities
- *
- * Description: Header file containing prototypes for the
- * file I/O project functions.
- */
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content);
+
+/**
+ * append_text_to_file - appends text at end of file
+ * @filename: name of the file
+ * @text_content: NULL-terminated string to append
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content);
+
+/**
+ * _copy - copies contents of one file into another
+ * @src: source filename
+ * @dest: destination filename
+ *
+ * Return: 0 on success or relevant error code
+ */
+int _copy(const char *src, const char *dest);
 
 #endif /* MAIN_H */
 
